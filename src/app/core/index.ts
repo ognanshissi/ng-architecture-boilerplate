f51db8016adc35d658d731e5728feba3843da933
Ambroise BAZIE <bazieambroise@gmail.com>
2020-04-13 10:47:30 +0000

    [UPD] adding indexes to enhance the import process

---
+++ src/app/core/index.ts
@@ -0,0 +1,5 @@
+export * from './core.module';
+export * from './models';
+export * from './services/abstract.service';
+export * from './services/auth.service';
+export * from './services/user.service';
